7832f6f88a46f9b99e754e80ab244cd8f0bd4e0e
Nihanth2025 <sk1441@gmail.com>
2026-04-21 19:20:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function add() {
+            var num1=Number(document.getElementById("number1").value)
+            var num2=Number(document.getElementById("number2").value)
+            var sol = num1 + num2;
+            document.getElementById("demo").innerHTML = sol;
+}
+function sub() {
+            var num1=Number(document.getElementById("number1").value)
+            var num2=Number(document.getElementById("number2").value)
+            var sol = num1 - num2;
+            document.getElementById("demo").innerHTML = sol;
+}
